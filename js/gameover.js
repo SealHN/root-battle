a97da69e0c834cb55e633c4ad080395e85c6043a
HNun <iuuoun0821@outlook.com>
2025-08-01 21:24:40 +0800

    Create gameover.js

---
+++ js/gameover.js
@@ -0,0 +1,13 @@
+// 游戏结束页面逻辑
+document.addEventListener('DOMContentLoaded', () => {
+    // 可以在这里添加一些动画效果
+});
+
+function restartGame() {
+    // 直接跳转到游戏页面，不显示说明
+    window.location.href = 'game.html';
+}
+
+function returnToMain() {
+    window.location.href = 'index.html';
+}
